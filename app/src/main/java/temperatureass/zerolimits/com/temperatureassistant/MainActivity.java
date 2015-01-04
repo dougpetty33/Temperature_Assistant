994c2b6f8ea1e157b52e91c5ff6f17e4fac26f72
dpetty <dpetty@control4.com>
2015-01-04 14:31:14 -0700

    Fixed network communication.

--- app/src/main/java/temperatureass/zerolimits/com/temperatureassistant/MainActivity.java
+++ app/src/main/java/temperatureass/zerolimits/com/temperatureassistant/MainActivity.java
@@ -19,25 +19,32 @@ import com.android.volley.toolbox.Volley;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 
+import org.apache.http.conn.util.InetAddressUtils;
+
 import java.io.IOException;
 import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.MulticastSocket;
 import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.*;
 
 public class MainActivity extends ActionBarActivity {
     public static final String NEWLINE = "\r\n";
     public static final int PORT = 1900;
-    public static final String HOST = "Host: 239.225.255.250:1900";
-    public static final String ADDRESS = "239.225.255.250";
+    public static final String HOST = "Host: 239.255.255.250:1900";
+    public static final String ADDRESS = "239.255.255.250";
     public static final String ST = "ST: ";
-   // public static final String LOCATION = "LOCATION";
-   // public static final String NT = "NT";
-   // public static final String NTS = "NTS";
-   // public static final String SL_NOTIFY = "NOTIFY * HTTP/1.1";
+    // public static final String LOCATION = "LOCATION";
+    // public static final String NT = "NT";
+    // public static final String NTS = "NTS";
+    // public static final String SL_NOTIFY = "NOTIFY * HTTP/1.1";
     public static final String SL_MSEARCH = "M-SEARCH * HTTP/1.1";
-   // public static final String SL_OK = "HTTP/1.1 200 OK";
-    public static final String ST_ContentDirectory = "schemas-upnp-org:service:TemperatureSensor:1";
+     public static final String SL_OK = "HTTP/1.1 200 OK";
+    public static final String ST_ContentDirectory = "zerolimits:sprinkler";
     public static final String USER_AGENT = "User-Agent: stuff";
     public static final String CONNECTION = "Connection: close";
 
@@ -47,32 +54,41 @@ public class MainActivity extends ActionBarActivity {
     public static final String NTS_DISCOVER = "MAN: \"ssdp:discover\"";
     public static final String MX = "MX: 5";
     public static final String TAG = "temp";
-  //  public static final String GRAPH_URL = "/graph.png";
+    //  public static final String GRAPH_URL = "/graph.png";
 
     private Button id;
     private Button graph;
     private EditText info;
     private WebView tempGraph;
+    private EditText debugTxt;
+    private volatile DatagramPacket sender = null;
+    private Object lock;
+    private String search = SL_MSEARCH + NEWLINE + MX + NEWLINE + ST + ST_ContentDirectory + NEWLINE + NTS_DISCOVER + NEWLINE + USER_AGENT + NEWLINE + CONNECTION + NEWLINE + HOST + NEWLINE + NEWLINE;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_main);
 
+
         id = (Button) findViewById(R.id.idBtn);
         graph = (Button) findViewById(R.id.grphBtn);
         Button getTemp = (Button) findViewById(R.id.crntTemp);
         info = (EditText) findViewById(R.id.dev_info);
+        debugTxt = (EditText) findViewById(R.id.dbgText);
+
+       Thread t = new Thread(new Listener());
+        t.start();
 
         id.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
 
-               DeviceFinder dfTask = new DeviceFinder();
-               DeviceListener dlTask = new DeviceListener();
-
-              dfTask.execute();
-              dlTask.execute();
+                sendPacket sp = new sendPacket();
+                sp.execute();
+                sp = null;
+                Log.d(TAG, "ID was pressed");
             }
         });
 
@@ -81,19 +97,17 @@ public class MainActivity extends ActionBarActivity {
             public void onClick(View v) {
 
                 RequestQueue que = Volley.newRequestQueue(MainActivity.this);
-                String url = "";
+                String url = "http://192.168.2.123/getTemp?units=c";
                 String message = "stuff";
 
                 StringRequest sr = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
                     @Override
-                public void onResponse(String response){
+                    public void onResponse(String response) {
                         //todo Parse response for temperature
-
                     }
                 }, new Response.ErrorListener() {
                     @Override
-                   public void onErrorResponse(VolleyError error){
-
+                    public void onErrorResponse(VolleyError error) {
                     }
                 });
                 que.add(sr);
@@ -103,20 +117,24 @@ public class MainActivity extends ActionBarActivity {
             }
         });
 
+
         graph.setOnClickListener(new View.OnClickListener() {
 
-            public void onClick(View v){
+            public void onClick(View v) {
                 tempGraph = (WebView) findViewById(R.id.tempGrph);
-                if(info.getText().toString() == "") {
+                if (info.getText().toString() == "") {
                     Log.d(TAG, "Need to enter an IP address");
                     Toast.makeText(MainActivity.this, "Please enter an IP address", Toast.LENGTH_LONG).show();
                     return;
                 }
+                else {
                     tempGraph.setVisibility(View.VISIBLE);
                     tempGraph.loadUrl("http://www.ampyourstrat.com/wp-content/uploads/android-logo.jpg");
+                }
             }
         });
     }
+
 
     private void openAlert(View view, String temperature) {
         final AlertDialog.Builder alertBuilder = new AlertDialog.Builder(MainActivity.this);
@@ -125,86 +143,72 @@ public class MainActivity extends ActionBarActivity {
         alertBuilder.setNeutralButton("Dismiss", new DialogInterface.OnClickListener() {
 
             @Override
-            public void onClick(DialogInterface di, int id){
-                    di.dismiss();
+            public void onClick(DialogInterface di, int id) {
+                di.dismiss();
             }
         });
         AlertDialog ad = alertBuilder.create();
         ad.show();
     }
 
-    class DeviceListener extends AsyncTask<Void, Void, String> {
-
+    class Listener implements Runnable {
         @Override
-        protected void onPreExecute(){
-            info.setText("Looking for response from temperature device.");
-        }
-
-        @Override
-        protected String doInBackground(Void... params) {
-            Log.d(TAG, "Execute");
-            byte[] buf = new byte[1024];
+        public void run(){
             try {
-                MulticastSocket clientMultiSock = new MulticastSocket(PORT);
-                clientMultiSock.joinGroup(InetAddress.getByName(ADDRESS));
-                Log.d(TAG, "Setup multicast socket");
-                while (true){
-                    DatagramPacket recv = new DatagramPacket(buf, 0, buf.length);
-                    clientMultiSock.receive(recv);
-
-                    final String msg = new String(recv.getData());
-                    Log.d(TAG, msg);
-                    if( msg.contains("M-SEARCH")){
-                        final String clientAddress = recv.getAddress().getHostAddress();
-                        clientMultiSock.disconnect();
-                        return clientAddress;
+                byte[] buffer = new byte[1024];
+                DatagramSocket sock = new DatagramSocket(1900);
+                final DatagramPacket incoming = new DatagramPacket(buffer, buffer.length);
+                while (true) {
+                    sock.receive(incoming);
+                    Log.d(TAG, "Receive from " + incoming.getAddress().getHostAddress());
+                    String response = new String(incoming.getData());
+                    if(response.contains(MainActivity.SL_OK) && response.contains(MainActivity.ST_ContentDirectory)){
+                      runOnUiThread(new Runnable() {
+                          @Override
+                          public void run() {
+                              MainActivity.this.info.setText(incoming.getAddress().getHostAddress());
+                          }
+                      });
                     }
                 }
-            } catch (IOException e) {
-                Log.e(TAG, "Could not send the packet");
+            } catch (SocketException e) {
+                e.printStackTrace();
+            } catch (IOException e){
+                e.printStackTrace();
             }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(String address){
-            info.setText(address);
         }
     }
 
-    class DeviceFinder extends AsyncTask<Void,Void,Void>
-    {
+    class sendPacket extends AsyncTask<Void, Void, Void> {
+
         @Override
         protected void onPreExecute(){
-            info.setText("Contacting temperature device");
+           MainActivity.this.info.setText("Looking for device..... ");
         }
 
         @Override
         protected Void doInBackground(Void... params){
-            MulticastSocket msock;
-            String query = SL_MSEARCH + NEWLINE + MX + NEWLINE + ST + ST_ContentDirectory + NEWLINE + NTS_DISCOVER + NEWLINE + USER_AGENT + NEWLINE + CONNECTION + NEWLINE + HOST + NEWLINE + NEWLINE;
             try {
-
-                msock = new MulticastSocket(PORT);
-                msock.setTimeToLive(5);
-                msock.joinGroup(InetAddress.getByName(ADDRESS));
-                msock.send(new DatagramPacket(query.getBytes(), query.getBytes().length, InetAddress.getByName(ADDRESS), PORT));
-                msock.setTimeToLive(5);
-                Log.d(TAG, "Sent the packet");
-                msock.leaveGroup(InetAddress.getByName(ADDRESS));
-
-            } catch (IOException e1) {
-                Log.e(TAG, "There was an error sending the packet");
-                e1.printStackTrace();
+                Log.d(TAG, "Generating send packet");
+                DatagramSocket ds = new DatagramSocket();
+                DatagramPacket sendPacket = new DatagramPacket(search.getBytes(), search.getBytes().length, InetAddress.getByName(ADDRESS), 1900);
+                ds.send(sendPacket);
+                ds.close();
+                ds = null;
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            } catch (SocketException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
             return null;
         }
 
-        @Override
-        protected void onPostExecute(Void result){
 
-        }
     }
 }
+
+
 
 
